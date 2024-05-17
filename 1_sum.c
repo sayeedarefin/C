8b7db54a5ce480534c212d547cbaaf23f194a6d4
sayeedarefin <sayeedarefin5@gmail.com>
2024-05-17 16:28:37 +0600

    first commit

---
+++ 1_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{   int a,b;
+    printf("Hello");
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    printf("The sum is:%d ", a+b);
+
+
+    return 0;
+}
