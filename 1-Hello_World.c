d15d5cf32f1a349ddb8af901abeb7540dfd8a55b
sayeedarefin <sayeedarefin5@gmail.com>
2024-07-27 06:28:43 +0600

    variable

---
+++ 1-Hello_World.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  printf("I am learning C.");
+  printf("And it is awesome!\n");
+
+  //To insert a new line, you can use the \n character:
+    printf("Where there is a will,\n");
+    printf("There is a way");
+
+//Two \n characters after each other will create a blank line:
+    printf("Where there is a will,\n\n");
+    printf("There is a way");
+  return 0;
+}
+
+//The newline character (\n) is called an escape sequence
---
+++ 2-variables.c
@@ -0,0 +1,4 @@
+// we need 5 things to declare a varible like javascript
+
+int num = 25;
+// but if we want to show the variable value, we canot print the value directly like other programming languages.
